1d82ffde2e8d8d3149d19c9a4a9a525c13fcd441
Ayoub_repo <58951594+abenhamdi@users.noreply.github.com>
2023-01-27 17:17:52 +0100

    Create getTotalBalance.js

---
+++ Alchemy Week 3/Ethereum Transactions/Total Wei/getTotalBalance.js
@@ -0,0 +1,24 @@
+const provider = require('./provider');
+
+async function getTotalBalance(addresses) {
+
+    let sum = 0;
+    for(let i = 0; i <= addresses.length; i++){
+        const response = await provider.send({
+        jsonrpc: "2.0",
+        id: 1,
+        method: "eth_getBalance",
+        params: [addresses[i], "latest"],
+        });
+
+        
+        sum += parseInt(response.result);
+    }   
+
+
+   return sum;
+    // return the total balance of all the addresses
+}
+
+
+module.exports = getTotalBalance;
