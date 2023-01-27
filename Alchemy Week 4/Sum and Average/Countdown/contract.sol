95901daa42569615bd36cf0553611d864e3b69fd
Ayoub_repo <58951594+abenhamdi@users.noreply.github.com>
2023-01-27 18:17:10 +0100

    Create contract.sol

---
+++ Alchemy Week 4/Sum and Average/Countdown/contract.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Contract {
+    uint counter = 10; 
+
+    function tick() public {
+        if(counter == 1){
+            selfdestruct(payable(msg.sender));
+        }
+        counter -- ;
+    }
+    
+}
