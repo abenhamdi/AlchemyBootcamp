e10f01de2f83fb5715205cace94d3d83830ae19d
Ayoub_repo <58951594+abenhamdi@users.noreply.github.com>
2023-01-27 18:13:27 +0100

    Create Sidekick.sol

---
+++ Alchemy Week 4/Address Interactions/Sending Data/Arbitrary Alert/Sidekick.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Sidekick {
+    function relay(address hero, bytes memory data) external {
+        (bool success, )= hero.call(data);
+        require(success);    
+
+        // send all of the data as calldata to the hero
+    }
+}
