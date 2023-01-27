27d9fdc54ea0b205fd9189e55ecf15a1ede4187c
Ayoub_repo <58951594+abenhamdi@users.noreply.github.com>
2023-01-27 17:55:52 +0100

    Create index.js

---
+++ Alchemy Week 4/Smart Contracts Communication/Contracts with ether.js/Signer/index.js
@@ -0,0 +1,14 @@
+/**
+ * Set the message on the contract using the signer passed in
+ *
+ * @param {ethers.Contract} contract - ethers.js contract instance
+ * @param {ethers.types.Signer} signer - ethers.js signer instance
+ * @return {promise} a promise of transaction modifying the `message`
+ */
+async function setMessage(contract, signer) {
+
+    await contract.connect(signer).modify("Contain anything other than empty string");
+    
+}
+
+module.exports = setMessage;
