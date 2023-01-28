2cd5ddeb507a0a221c7aa39adb90151179077b83
Ayoub_repo <58951594+abenhamdi@users.noreply.github.com>
2023-01-28 23:31:01 +0100

    Create PrimeGame.sol

---
+++ Alchemy Week 7/Libraries/Smart Contract Libraries/Next Prime/PrimeGame.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./Prime.sol";
+
+contract PrimeGame {
+    function isWinner() external view returns (bool){
+        return Prime.isPrime(block.number);
+    }
+    
+}
